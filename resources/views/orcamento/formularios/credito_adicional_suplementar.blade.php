57cea54da370a9582223cf9072c4ba9b0f500fcf
samuelpaes <samuelpaes@hotmail.com>
2020-02-19 12:00:08 -0300

    Update credito_adicional_suplementar.blade.php

--- resources/views/orcamento/formularios/credito_adicional_suplementar.blade.php
+++ resources/views/orcamento/formularios/credito_adicional_suplementar.blade.php
@@ -392,10 +392,10 @@
 											<table class="table table-hover table-striped" id="tabela_anular" name="tabela_anular" style='font-size:98%'>
 												<thead>
 													<tr style="height:100px">
-														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 90px; line-height: 1.5; text-align:center; color:#000;"><b>Unidade Executora</b></label></th>
-														<th style="text-align:center; "><label style="flex-wrap:nowrap; display: inline-block; width: 120px; line-height: 1.5; text-align:center; color:#000"><b>Classificação Funcional Programática</b></label></th>
-														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 100px; line-height: 1.5; text-align:center; color:#000"><b>Natureza De Despesa</b></label></th>
-														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 100px; line-height: 1.5; text-align:center; color:#000;  margin-left:-15px;"><b>Vínculo</b></label></th>
+														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 90px; line-height: 1.5; text-align:center; color:#000;  margin-left:5px;"><b>Unidade Executora</b></label></th>
+														<th style="text-align:center; "><label style="flex-wrap:nowrap; display: inline-block; width: 120px; line-height: 1.5; text-align:center; color:#000;  margin-left:5px;"><b>Classificação Funcional Programática</b></label></th>
+														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 100px; line-height: 1.5; text-align:center; color:#000; margin-left:5px;"><b>Natureza De Despesa</b></label></th>
+														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 100px; line-height: 1.5; text-align:center; color:#000;  margin-left:-10px;"><b>Vínculo</b></label></th>
 														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 50px; line-height: 1.5; text-align:center; color:#000; margin-left:-15px;"><b>Dotação</b></label></th>
 														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 100px; line-height: 1.5; text-align:center; color:#000"><b>Valor</b></label></th>
 														<th style=""><label style="flex-wrap:nowrap; display: inline-block; width: 100px; line-height: 1.5; text-align:center; color:#000"><b>Recurso</b></label></th>
