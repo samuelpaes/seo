95bf5f5a905e5125026874246592a98ed7bc3a46
samuelpaes <samuelpaes@hotmail.com>
2020-06-05 19:29:02 -0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -390,9 +390,6 @@ background: rgba(255, 255, 255, 0.4);
 				<main class="py-4"  style="z-index:0">
 					<br>
 
-					<div class="alert alert-primary" role="alert">
-  This is a primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
-</div>
 					@if(auth()->user()->isAdmin == 0 || auth()->user()->isAdmin == 1 || auth()->user()->isAdmin == 2)
 						@yield('content')
 					@else<!-- Se o usuário não tem acesso, chama o modal sem acesso -->	
@@ -479,6 +476,52 @@ $(document).ready(function() {
         notificationsWrapper.find('.notif-count').text(notificationsCount);
         notificationsWrapper.show();
 
-		$('#alert alert-info').show();
-      });
+		$.notify({
+			// options
+			//icon: 'glyphicon glyphicon-warning-sign',
+			title: 'AVISO',
+			message: notificacao,
+			//url: 'https://github.com/mouse0270/bootstrap-notify',
+			//target: '_blank'
+		},
+			{
+				// settings
+				element: 'body',
+				position: null,
+				type: "info",
+				allow_dismiss: true,
+				newest_on_top: false,
+				showProgressbar: false,
+				placement: {
+					from: "top",
+					align: "right"
+				},
+				offset: 20,
+				spacing: 10,
+				z_index: 1031,
+				delay: 5000,
+				timer: 1000,
+				url_target: '_blank',
+				mouse_over: null,
+				animate: {
+					enter: 'animated fadeInDown',
+					exit: 'animated fadeOutUp'
+				},
+				onShow: null,
+				onShown: null,
+				onClose: null,
+				onClosed: null,
+				icon_type: 'class',
+				template: '<div data-notify="container" class="col-xs-11 col-sm-3 alert alert-{0}" role="alert">' +
+					'<button type="button" aria-hidden="true" class="close" data-notify="dismiss">×</button>' +
+					'<span data-notify="icon"></span> ' +
+					'<span data-notify="title">{1}</span> ' +
+					'<span data-notify="message">{2}</span>' +
+					'<div class="progress" data-notify="progressbar">' +
+						'<div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div>' +
+					'</div>' +
+					'<a href="{3}" target="{4}" data-notify="url"></a>' +
+				'</div>' 
+			});
+      	});
     </script>
